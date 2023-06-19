7d20f4d7bb6789c740ad2e44e58f8f6c6c40aa5f
stefanolaguda <lagudadev@gmail.com>
2023-06-19 12:06:26 +0200

    added static FormGroup and function that get dynamic fields of input object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dynamic-form_ad';
+export class AppComponent implements OnInit {
+  registerForm!: FormGroup;
+  fields: string[] = [];
+  model = {
+    name: '',
+    lastName: '',
+    address: '',
+    age: '',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.buildForm();
+    this.getFormControlsFields();
+  }
+
+  buildForm() {
+    this.registerForm = new FormGroup({
+      name: new FormControl(''),
+      lastName: new FormControl(''),
+      age: new FormControl(''),
+    });
+  }
+
+  getFormControlsFields() {
+    const formGroupFields: Record<string, FormControl> = {};
+
+    for (const field of Object.keys(this.model)) {
+      formGroupFields[field] = new FormControl('');
+      this.fields.push(field);
+    }
+
+    console.log(formGroupFields);
+
+    return formGroupFields;
+  }
 }
