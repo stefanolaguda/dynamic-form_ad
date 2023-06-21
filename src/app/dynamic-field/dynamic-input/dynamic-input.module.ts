a756bd85667522985487f9682cfe4618fe4809c0
stefanolaguda <lagudadev@gmail.com>
2023-06-21 17:02:41 +0200

    fixed error messages

--- src/app/dynamic-field/dynamic-input/dynamic-input.module.ts
+++ src/app/dynamic-field/dynamic-input/dynamic-input.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicInputComponent } from './dynamic-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DynamicErrorModule } from '../dynamic-error/dynamic-error.module';
 
 @NgModule({
   declarations: [DynamicInputComponent],
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, DynamicErrorModule],
   exports: [DynamicInputComponent],
 })
 export class DynamicInputModule {}
